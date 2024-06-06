31d8217f39c47f961b473c19884dae69ee3536d5
Mikonimo <mikonani413@gmail.com>
2024-06-06 14:11:05 +0300

    Task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,20 +14,20 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *left_child;
 
 	if (parent == NULL)
-    {
-        return (NULL);
-    }
-    left_child = binary_tree_node(parent, value);
-    if (left_child == NULL)
-    {
-        return (NULL);
-    }
-    if (parent->left != NULL)
-    {
-        left_child->left = parent->left;
-        parent->left->parent = left_child;
-    }
-    parent->left = left_child;
+	{
+		return (NULL);
+	}
+	left_child = binary_tree_node(parent, value);
+	if (left_child == NULL)
+	{
+		return (NULL);
+	}
+	if (parent->left != NULL)
+	{
+		left_child->left = parent->left;
+		parent->left->parent = left_child;
+	}
+	parent->left = left_child;
 
-	return (left_child);    
+	return (left_child);
 }
