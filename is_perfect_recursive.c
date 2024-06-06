6962ced183c6f7c564681bac6210827ae6f38883
Mikonimo <mikonani413@gmail.com>
2024-06-06 22:36:13 +0300

    task

---
+++ is_perfect_recursive.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * is_perfect_recursive - Helper function to check if tree is perfect
+ * @tree: Pointer to the root node of the tree to check
+ * @depth: Depth of the leftmost leaf
+ * @level: Current level of the node
+ * Return: 1 if tree is perfect, 0 otherwise
+ */
+int is_perfect_recursive(const binary_tree_t *tree, size_t depth, size_t level)
+{
+	if (tree == NULL)
+		return (1);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (depth == level + 1);
+
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+
+	return (is_perfect_recursive(tree->left, depth, level + 1) &&
+			is_perfect_recursive(tree->right, depth, level + 1));
+}
